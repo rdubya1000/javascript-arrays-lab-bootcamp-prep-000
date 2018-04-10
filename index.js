0d206a73b6d22fe2bf4991a02e2b16d51491b26a
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-10 20:26:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten() {
+  kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(){
+  return [...kittens, 'Broom'];
+}
+
+function prependKitten(){
+  return ['Arnold', ...kittens];
+}
+
+function removeLastKitten(){
+   return kittens.slice(-2);
+}
